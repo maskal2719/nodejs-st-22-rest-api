2651c0a914c11dcc6aeb5489d5d1287594e5d79a
Stanislav Moskalenko <maskal2719@gmail.com>
2022-08-02 13:29:10 +0300

    add task 4-2

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './services/users.service';
-import { UsersController } from './users.controller';
+import { UsersController } from './controllers/users.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './models/users.model';
 
