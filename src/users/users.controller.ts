7aea206c4410b0b04b760c638ee9729c0ee70154
Stanislav Moskalenko <maskal2719@gmail.com>
2022-08-08 16:55:37 +0300

    add migration

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,37 +1,48 @@
-/* eslint-disable prettier/prettier */
-import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './services/users.service';
 
 @Controller('v1/users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
 
-    constructor(private readonly usersService : UsersService) {
-    }
-    
-    @Get()
-    getAll() {
-        return this.usersService.getAllUsers();
-    }
+  @Get()
+  getAll(
+    @Query('loginSubstring') loginSubstring: string,
+    @Query('limit') limit: number,
+  ) {
+    return this.usersService.getAllUsers(loginSubstring, limit);
+  }
 
-    @Get(':id')
-    getOne(@Param('id') id : string) {
-        return this.usersService.getOne(id);
-    }
-    
-    @Post()
-    create(@Body() createUserDto : CreateUserDto) {
-        return this.usersService.createUser(createUserDto);
-    }
+  @Get(':id')
+  getOne(@Param('id') id: string) {
+    return this.usersService.getOne(id);
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.usersService.remove(id);
-    }
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.createUser(createUserDto);
+  }
 
-    @Put(':id')
-    update(@Body() updateUserDto : UpdateUserDto, @Param('id') id : string) {
-        return this.usersService.update(id, updateUserDto);
-    }
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(id);
+  }
+
+  @Put(':id')
+  update(@Body() updateUserDto: UpdateUserDto, @Param('id') id: string) {
+    return this.usersService.update(id, updateUserDto);
+  }
 }
